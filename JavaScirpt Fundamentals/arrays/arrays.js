cf37f2273d28e328d5f14e22c7d2b46ad0ec30d5
Nemanja0033 <antonijevicnemanja68@gmail.com>
2024-10-12 21:02:44 +0200

    arrays

---
+++ JavaScirpt Fundamentals/arrays/arrays.js
@@ -0,0 +1,8 @@
+var cars = ["Ford", "Mazda"]; 
+cars[1] = "BMW"; 
+console.log(cars[0]); 
+console.log(cars[1]);
+
+var numbers = [1, 2, 55, 23, 0, 11, 123, -33]; 
+numbers.sort(); 
+document.write(numbers.toString());
