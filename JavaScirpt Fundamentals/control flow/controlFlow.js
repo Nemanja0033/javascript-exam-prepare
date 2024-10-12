01d5270f9e33a85e06afc59099d115918279ba7d
Nemanja0033 <antonijevicnemanja68@gmail.com>
2024-10-12 20:56:22 +0200

    flow control

---
+++ JavaScirpt Fundamentals/control flow/controlFlow.js
@@ -0,0 +1,49 @@
+/*var speed = 9;
+ 
+if (speed < 10) { 
+console.log("Too slow..."); 
+}
+
+if (condition) { 
+    statement_1; 
+    } else { 
+    statement_2; 
+    }
+
+if (condition_1) { 
+statement_1; 
+} else if (condition_2) { 
+statement_2; 
+} else if (condition_n) { 
+statement_n; 
+} else { 
+statement_last; 
+}
+
+var speed = 50; 
+ 
+if (speed < 10) { 
+console.log("Too slow..."); 
+ 
+} else if (speed <= 80) { 
+console.log("Regular speed."); 
+ 
+} else if (speed < 100) { 
+console.log("Too fast!"); 
+ 
+} else { 
+console.log("Incorrect value"); 
+}*/
+
+switch (expression) { 
+    case label_1: 
+    statements_1 
+    break; 
+    case label_2: 
+    statements_2 
+    break; 
+
+    default: 
+    statements_def 
+    break; 
+    }
