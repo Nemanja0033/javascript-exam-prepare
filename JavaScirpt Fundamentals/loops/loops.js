aeec8e6a962ba66b578447df93cb759017b92247
Nemanja0033 <antonijevicnemanja68@gmail.com>
2024-10-12 20:59:55 +0200

    loops

---
+++ JavaScirpt Fundamentals/loops/loops.js
@@ -0,0 +1,30 @@
+for (var i = 0; i < 5; i++) {  // for loop
+    console.log("Hello World");
+    }
+    console.log(i);
+
+    var x = 0; 
+    while (x < 5) {
+     
+    console.log("Hello World"); 
+    x = x + 1; 
+    }
+    
+    for (var i = 0; i < 100; i++) { 
+        if (i > 50) { 
+        break;   // break key word
+        } 
+        console.log(i); 
+        } 
+        console.log(i);
+
+        
+        for (var i = 0; i < 100; i++) { 
+            if (i > 50) { 
+            continue; 
+            } 
+                           //countinue keyword
+            console.log(i); 
+            } 
+            
+            console.log(i);
